cac0a7bf13ea0f4408399c964cea847b32a1011b
sk0rp <asawoniewicz@gmail.com>
2017-02-26 22:59:04 +0100

    first page

--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -1,9 +1,11 @@
 //jQuery to collapse the navbar on scroll
 $(window).scroll(function() {
     if ($(".navbar").offset().top > 50) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
+		$("#logo img").addClass("logo-collapse");
+		$("#logo img").attr("src", "img/logo_latin_soul_full.png");
     } else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+		$("#logo img").removeClass("logo-collapse");
+		$("#logo img").attr("src", "img/logo_latin_soul_big.png");
     }
 });
 
