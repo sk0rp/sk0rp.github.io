9fcc5307e59c96d33fa3f9376d56d3936a35d9a9
sk0rp <asawoniewicz@gmail.com>
2017-03-01 01:26:22 +0100

    hide full logo

--- js/site.js
+++ js/site.js
@@ -1,11 +1,13 @@
 function logoCollapse() {
 	$("#logo img").attr("src", "img/logo_latin_soul_full.png");
 	$("#logo img").addClass("logo-collapse");
+	$("#logo2").fadeOut("slow");
 }
 
 function logoRecover() {
 	$("#logo img").removeClass("logo-collapse");
 	$("#logo img").attr("src", "img/logo_latin_soul_big_en.png");
+	$("#logo2").fadeIn("slow");
 }
 
 $('.nav a').on('click', function(){
