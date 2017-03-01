e1ab9711be0b752fbfd7b2d2406f666d06e8431f
sk0rp <asawoniewicz@gmail.com>
2017-03-01 01:28:23 +0100

    hide full logo

--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -1,6 +1,6 @@
 //jQuery to collapse the navbar on scroll
 $(window).scroll(function() {
-    if ($(".navbar").offset().top > 100) {
+    if ($(".navbar").offset().top > 50) {
 		logoCollapse();
 		$("#social-bar.collapsed").fadeOut('slow');
     } else {
